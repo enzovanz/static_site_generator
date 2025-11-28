fea90f772dfacf5488c64e665a54b9cf58484f32
enzovanz <enzovanzuita@gmail.com>
2025-11-27 22:27:42 -0300

    Recursive function cleanup

--- src/main.py
+++ src/main.py
@@ -2,29 +2,19 @@ import os
 import shutil
 
 def copy_files(src, dest):
-    src = os.path.join(os.getcwd(), src)
-    dest = os.path.join(os.getcwd(), dest)
     if os.path.exists(dest):
         shutil.rmtree(dest)
     os.mkdir(dest)
-    for item in os.listdir(src):
-        file_path_source = os.path.join(src, item)
-        file_path_destination = os.path.join(dest, item)
-        if os.path.isfile(file_path_source):
-            shutil.copy(file_path_source, dest)
-        else:
-            os.mkdir(os.path.join(dest, item))
-            recursive_copy(file_path_source, file_path_destination)
-        
+    recursive_copy(src, dest)
 
 def recursive_copy(src, dest):
     for item in os.listdir(src):   
         file_path_source = os.path.join(src, item)
         file_path_destination = os.path.join(dest, item)
         if os.path.isfile(file_path_source):
-            shutil.copy(file_path_source, dest)
+            shutil.copy(file_path_source, file_path_destination)
         else:
-            os.mkdir(os.path.join(dest, item))
+            os.mkdir(file_path_destination)
             recursive_copy(file_path_source, file_path_destination)
 
 def main():
